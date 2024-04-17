27a56319f545afc876ab685b24323d2db71fa6bf
the-eris-net <the.eris.net@gmail.com>
2024-04-17 18:01:18 +0900

    feat:test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,4 @@ docker load < /home/ec2-user/deploy/groom-test-docker.tar
 docker load < /home/ec2-user/deploy/groom-test-docker-latest.tar
 docker images
 docker run -d -p 80:80 --name groom-test-docker groom-test-docker
-docker save groom-test-docker:latest > groom-test-docker-latest.tar
+#docker save groom-test-docker:latest > groom-test-docker-latest.tar
